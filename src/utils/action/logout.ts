67f906ec66e85f0470361503a7422d99f66d25be
HANKSIE <32055793+HANKSIE@users.noreply.github.com>
2021-08-12 00:29:13 +0800

    fix: logout 觸發未跳轉回login

--- src/utils/action/logout.ts
+++ src/utils/action/logout.ts
@@ -1,9 +1,15 @@
+import { useStore } from "@/store";
 import authApi from "@/utils/api/auth";
 import router from "@/router";
+import apiToken from "@/utils/token/apiToken";
 
 export default async () :Promise<void> => {
+ 
     try {
       await authApi.logout();
+      const { auth } = useStore();
+      auth.removeUser();
+      apiToken.remove();
       router.push({name: "login"});
     } catch (err) {
       console.error(err);
